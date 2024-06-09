ab2e835cf017056588614efda376c12bb56e0004
Benboudina Moussa <121502039+MoussaBenboudina@users.noreply.github.com>
2024-06-09 13:30:47 -0700

    Delete Kyu5 directory

--- Kyu5/Calculate age in years.js
+++
@@ -1,9 +0,0 @@
-function getAge(birthdate, now = new Date()) {
-  let age = now.getFullYear() - birthdate.getFullYear();
-  const monthDifference = now.getMonth() - birthdate.getMonth();
-  const dayDifference = now.getDate() - birthdate.getDate();
-  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
-    age--;
-  }
-  return age;
-}
